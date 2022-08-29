83f18b8f031f412a842473d419ceb261284af704
Francisco Costa <francisco100eg@gmail.com>
2022-08-29 07:01:16 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -8,19 +8,55 @@ class App extends React.Component {
   state = {
     cardName: '',
     cardDescription: '',
-    cardAttr1: '',
-    cardAttr2: '',
-    cardAttr3: '',
+    cardAttr1: 0,
+    cardAttr2: 0,
+    cardAttr3: 0,
     cardImage: '',
     cardRare: '',
     cardTrunfo: '',
-    isSaveButtonDisabled: false,
+    isSaveButtonDisabled: true,
+  };
+
+  numeroAtributo = (numero) => {
+    const maximo = 90;
+    if (Number(numero) >= 0 && Number(numero) <= maximo) {
+      return true;
+    }
   };
 
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => {
+      const {
+        cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardImage,
+        cardRare,
+      } = this.state;
+      const attr1 = this.numeroAtributo(cardAttr1);
+      const attr2 = this.numeroAtributo(cardAttr2);
+      const attr3 = this.numeroAtributo(cardAttr3);
+      const soma = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+      const max = 210;
+      if (
+        cardName
+        && cardDescription
+        && cardImage
+        && cardRare
+        && soma <= max
+        && attr1
+        && attr2
+        && attr3
+      ) {
+        this.setState({ isSaveButtonDisabled: false });
+      } else {
+        this.setState({ isSaveButtonDisabled: true });
+      }
+    });
   };
 
   render() {
